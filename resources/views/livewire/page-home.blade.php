fffa8f018cdff203f8ab7ca41915d83b82f656dc
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-02-09 22:11:34 -0300

    Colocando conteudo em colunas e icone de lixeira.

--- resources/views/livewire/page-home.blade.php
+++ resources/views/livewire/page-home.blade.php
@@ -18,16 +18,20 @@
         @endif
         <livewire:turma-component />
         <div class="mx-8"><span>Qtde: {{count($turmas)}}</span></div>
-        @forelse ($turmas as $index => $turma)
-            <div class="gf bg-white mx-8 rounded mt-4 border-b-2 border-cyan-800 p-4 flex justify-center items-center gap-7">
-                <h3>{{$turma['turma']}}: {{$turma['quantidade']}}</h3>
-                <button wire:click="deletar({{$index}},{{$turma->id}})" class="gf bg-red-600 p-1 text-white rounded">deletar</button>
-                <livewire:button-check :wire:key="$index" />
-            </div>
-        @empty
-            <p class="bg-white mx-8 rounded mt-4 p-4">Por favor insira a quantidade de alunos nas turmas.</p>
-        @endforelse
 
+        <div class="grid grid-cols-2 gap-2">
+            @forelse ($turmas as $index => $turma)
+                <div class="gf bg-white mx-8 rounded mt-4 border-b-2 border-cyan-800 p-4 flex justify-center items-center w-10/12 gap-1">
+                    <h3>{{$turma['turma']}}: {{$turma['quantidade']}}</h3>
+                    <button wire:click="deletar({{$index}},{{$turma->id}})" class="gf bg-red-600 p-1 text-white rounded mr-2">
+                        <img class="w-6 h-6 text-gray-50" src="{{asset('img/trash-svgrepo-com.svg')}}" alt="deletar">
+                    </button>
+                    <livewire:button-check :wire:key="$index" />
+                </div>
+            @empty
+                <p class="bg-white mx-8 rounded mt-4 p-4">Por favor insira a quantidade de alunos nas turmas.</p>
+            @endforelse
+        </div>
     </div>
 
 </div>
