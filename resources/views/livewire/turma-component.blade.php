a4d3a476d3ee77a4399a4d4f3e2e5ff235f45eba
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-02-09 22:33:56 -0300

    Mudança no designer do componente de inserção das turmas.

--- resources/views/livewire/turma-component.blade.php
+++ resources/views/livewire/turma-component.blade.php
@@ -3,17 +3,17 @@
     @if (session()->has('message'))
         <div class=" bg-yellow-100 p-2 mx-8 my-2">{{session('message')}}</div>
     @endif
-    <div class="gf mx-8 flex justify-center gap-1 mb-3 pt-3">
-        <select wire:model="turmaSelect.turma" name="turma" id="turma">
-            <option value="null" selected>Selecione</option>
+    <div class="gf mx-8 mb-3 pt-3">
+        <select wire:model="turmaSelect.turma" class="w-full mb-2" name="turma" id="turma">
+            <option value="null" selected>Selecione a turma</option>
             @foreach ($turmas as $turma)
                 <option value="{{$turma}}">{{$turma}}</option>
             @endforeach
         </select>
     
-        <input wire:model="turmaSelect.quantidade" type="number" placeholder="quantidade">
+        <input wire:model="turmaSelect.quantidade" class="w-full mb-2" type="number" placeholder="quantidade">
     
-        <button wire:click="addTurma" class="gf bg-green-600 p-2 rounded text-white">OK</button>
+        <button wire:click="addTurma" class="w-full mb-2 bg-green-600 p-2 rounded text-white">OK</button>
     </div>
 
 </div>
