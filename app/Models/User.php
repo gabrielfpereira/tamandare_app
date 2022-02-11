833022a0b8df1e8005e3370d0cd29752a229c568
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-02-11 20:03:57 -0300

    Criando relacionamento com contador.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function turmas()
+    {
+        return $this->hasMany(Contador::class);
+    }
 }
